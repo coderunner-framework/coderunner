420255724a857427f775a42e22dfe102daa0a6fd
Edmund Highcock <github@edmundhighcock.com>
2013-03-22 10:10:05 +0000

    Version bump to 0.12.2

---
+++ lib/coderunner/system_modules/edison.rb
@@ -0,0 +1,13 @@
+class CodeRunner
+	require SCRIPT_FOLDER + '/system_modules/moab.rb'
+	module Edison
+		include Moab
+		#def batch_script
+			#raise "Please specify project" unless @project
+			#super
+		#end
+		def max_ppn
+			16
+		end
+	end
+end
