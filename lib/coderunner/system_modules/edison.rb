04f7edc1375f541f4b35c3b37861492bf976f95d
ferdinandvwyk <ferdinandvwyk@gmail.com>
2013-10-21 16:10:34 +0100

    --Ensure submission on Edison is to regular queue with PBS flag

--- lib/coderunner/system_modules/edison.rb
+++ lib/coderunner/system_modules/edison.rb
@@ -1,11 +1,11 @@
-class CodeRunner
+cass CodeRunner
 	require SCRIPT_FOLDER + '/system_modules/moab.rb'
 	module Edison
 		include Moab
-		#def batch_script
+ 		def batch_script
 			#raise "Please specify project" unless @project
-			#super
-		#end
+			"#PBS -q regular\n" + super
+		end
 		def max_ppn
 			16
 		end
