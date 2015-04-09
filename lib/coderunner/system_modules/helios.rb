b16e439ae4b0ffc5a2d6bf92df01e90184ce86cf
Edmund Highcock <edmundhighcock@users.sourceforge.net>
2015-04-09 15:57:26 +0100

    --Updated launcher for helios

--- lib/coderunner/system_modules/helios.rb
+++ lib/coderunner/system_modules/helios.rb
@@ -2,10 +2,6 @@ class CodeRunner
 	require SCRIPT_FOLDER + '/system_modules/slurm.rb'
 	module Helios
 		include Slurm
-		#def batch_script
-			#raise "Please specify project" unless @project
-			#super
-		#end
 		def max_ppn
 			16
 		end
@@ -14,13 +10,12 @@ class CodeRunner
       nprocstot = nodes.to_i * ppn.to_i
       "srun -n #{nprocstot}"
     end 
-    def run_command
-  # 		"qsub #{batch_script_file}"
-      if (ENV['CODE_RUNNER_LAUNCHER'].size > 0 rescue false)
-        return %[#@preamble #{mpi_prog} #{executable_location}/#{executable_name} #{parameter_string} > #{output_file} 2> #{error_file}]
-      else
-        "#@preamble #{mpi_prog}  #{executable_location}/#{executable_name} #{parameter_string}"
-      end
-    end
+	def run_command
+		if use_launcher
+			return %[#{code_run_environment}\n #@preamble #{mpi_prog}  #{executable_location}/#{executable_name} #{parameter_string} > #{output_file} 2> #{error_file}]
+		else
+			"#@preamble #{mpi_prog}  #{executable_location}/#{executable_name} #{parameter_string}"
+		end
+	end
 	end
 end
