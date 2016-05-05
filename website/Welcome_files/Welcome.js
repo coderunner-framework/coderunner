c77dee8667b9d0ce7359429924f3b485bbe4794d
Edmund Highcock <edmundhighcock@users.sourceforge.net>
2016-05-05 18:52:06 +0100

    --Added old website

---
+++ website/Welcome_files/Welcome.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.1 local-build-20100409
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Welcome_files/WelcomeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
