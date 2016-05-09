6c9fa5a5b477bac8823d3945da8a4618e229d7e3
ferdinandvanwyk <ferdinandvwyk@gmail.com>
2016-05-09 18:08:07 +0100

    update ncurses to 6.0

--- dependencies/download_dependencies.sh
+++ dependencies/download_dependencies.sh
@@ -12,7 +12,7 @@ done
 
 wget -nc ftp://ftp.gnu.org/gnu/gsl/gsl-2.1.tar.gz -P $DIR
 wget -nc https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.16/src/hdf5-1.8.16.tar.gz -P $DIR
-wget -nc ftp://ftp.gnu.org/gnu/ncurses/ncurses-5.9.tar.gz -P $DIR
+wget -nc ftp://ftp.gnu.org/gnu/ncurses/ncurses-6.0.tar.gz -P $DIR
 wget -nc ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.4.0.tar.gz -P $DIR
 wget -nc ftp://ftp.gnu.org/gnu/readline/readline-6.2.tar.gz -P $DIR
 wget -nc http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz -P $DIR
