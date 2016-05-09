69246550087f345280dacee39f27b08960eb250c
ferdinandvanwyk <ferdinandvwyk@gmail.com>
2016-05-09 18:14:07 +0100

    update yaml to 0.1.6

--- dependencies/download_dependencies.sh
+++ dependencies/download_dependencies.sh
@@ -15,6 +15,6 @@ wget -nc https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.16/src/hdf5-1.8.16.
 wget -nc ftp://ftp.gnu.org/gnu/ncurses/ncurses-6.0.tar.gz -P $DIR
 wget -nc ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.4.0.tar.gz -P $DIR
 wget -nc ftp://ftp.gnu.org/gnu/readline/readline-6.3.tar.gz -P $DIR
-wget -nc http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz -P $DIR
+wget -nc http://pyyaml.org/download/libyaml/yaml-0.1.6.tar.gz -P $DIR
 wget -nc https://cache.ruby-lang.org/pub/ruby/ruby-2.3.0.tar.gz -P $DIR
 
