bfb232139ea9d1c4a79e4f94c2a3fa311d6c6552
Edmund Highcock <edmundhighcock@users.sourceforge.net>
2017-01-10 14:50:08 +0100

    --Added custom batch_script for marconi

--- lib/coderunner/system_modules/marconi.rb
+++ lib/coderunner/system_modules/marconi.rb
@@ -9,5 +9,30 @@ class CodeRunner
 		def max_ppn
 			36
 		end
+	def batch_script
+		ppn_checks
+		hours, mins, secs = hours_minutes_seconds
+<<EOF
+	#!/bin/bash --login 
+	#PBS -N #{executable_name}.#{job_identifier}
+	#PBS -l ncpus=#{nprocstot}
+	#PBS -l mpiprocs=#{nprocstot}
+	#PBS -l select=#{nodes}
+	#PBS -l walltime=#{sprintf("%02d:%02d:%02d", hours, mins, secs)}
+	#{@project ? "#PBS -A #@project" : ""}
+	#{@queue ? "#PBS -q #@queue" : ""}
+
+  #{code_run_environment}
+
+	### start of jobscript 
+	cd $PBS_O_WORKDIR 
+	echo "workdir: $PBS_O_WORKDIR" 
+
+	echo "Submitting #{nodes}x#{ppn} job on #{CodeRunner::SYS} for project #@project..."
+EOF
+	end
+	def mpi_prog
+		"mpirun -np #{nprocstot}"
+	end
 	end
 end
