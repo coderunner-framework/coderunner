624845a8e28d5b5ea6916b823c44869b16f036b3
Edmund Highcock <edmundhighcock@users.sourceforge.net>
2018-02-06 11:41:15 +0100

    --Updated Marconi module to use slurm

--- lib/coderunner/system_modules/marconi.rb
+++ lib/coderunner/system_modules/marconi.rb
@@ -1,36 +1,25 @@
 class CodeRunner
-	require SCRIPT_FOLDER + '/system_modules/moab.rb'
+	require SCRIPT_FOLDER + '/system_modules/slurm.rb'
 	module Marconi
-		include Moab
-		def batch_script
-			raise "Please specify project" unless @project
-			super
-		end
+		include Slurm
+		#def batch_script
+			#raise "Please specify project" unless @project
+			#super
+		#end
 		def max_ppn
-			36
+			48
 		end
-	def batch_script
-		ppn_checks
-		hours, mins, secs = hours_minutes_seconds
-<<EOF
-	#!/bin/bash --login 
-	#PBS -N #{executable_name}.#{job_identifier}
-	#PBS -l select=#{nodes}:ncpus=#{ppn}:mpiprocs=#{ppn}
-	#PBS -l walltime=#{sprintf("%02d:%02d:%02d", hours, mins, secs)}
-	#{@project ? "#PBS -A #@project" : ""}
-	#{@queue ? "#PBS -q #@queue" : ""}
-
-  #{code_run_environment}
-
-	### start of jobscript 
-	cd $PBS_O_WORKDIR 
-	echo "workdir: $PBS_O_WORKDIR" 
-
-	echo "Submitting #{nodes}x#{ppn} job on #{CodeRunner::SYS} for project #@project..."
-EOF
-	end
-	def mpi_prog
-		"mpirun -np #{nprocstot}"
-	end
+		def run_command
+	# 		"qsub #{batch_script_file}"
+			if use_launcher
+				return %[#{code_run_environment}\n ibrun -n #{@nprocs} -o 0 #{executable_location}/#{executable_name} #{parameter_string} > #{output_file} 2> #{error_file}]
+			else
+				"srun #{executable_location}/#{executable_name} #{parameter_string}"
+			end
+		end
+		def batch_script
+			raise "Please specify the queue to submit to using the -Q (or Q:) flag" unless @queue
+			super
+		end	
 	end
 end
